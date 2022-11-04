398dccd25507cbe247fbc13c32553fa0f354c244
martinlopez1234 <martin.lr@hotmail.com>
2022-11-04 20:57:23 +0000

    My first commit

--- src/models.py
+++ src/models.py
@@ -8,23 +8,45 @@ from eralchemy import render_er
 
 Base = declarative_base()
 
-class Person(Base):
-    __tablename__ = 'person'
-    # Here we define columns for the table person
-    # Notice that each column is also a normal Python instance attribute.
+class Usuarios(Base):
+    __tablename__ = 'usuarios'
+    id = Column(Integer, primary_key=True)
+    username = Column(String(250), nullable=False, unique= True)
+    password = Column(String(100), nullable=False)
+    apellido = Column(String(250), nullable= False)
+    email = Column(String (100), nullable=False, unique = True)
+    fecha_de_subscripción = Column(Integer,nullable=False)
+    favorites = relationship('Favoritos')
+
+class Favoritos(Base):
+    __tablename__ = 'favoritos'
+    id_usuario = Column(Integer, ForeignKey('usuarios.id'), nullable= False, primary_key=True)
+    id_favoritos = Column(Integer, ForeignKey('personajes.id'), ForeignKey('planetas.id'))
+    fav_name = Column(String(250), ForeignKey('personajes.name'), ForeignKey('planetas.name'))
+    fav_tema = Column(String(250), ForeignKey('personajes.__tablename__'), ForeignKey('planetas.__tablename__'))
+
+
+class Personajes(Base):
+    __tablename__ = 'personajes'
     id = Column(Integer, primary_key=True)
     name = Column(String(250), nullable=False)
+    gender = Column(String(250))
+    hair_color = Column(String(250))
+    eye_color = Column(String(250))
+ 
 
-class Address(Base):
-    __tablename__ = 'address'
-    # Here we define columns for the table address.
-    # Notice that each column is also a normal Python instance attribute.
+
+class Planetas(Base):
+    __tablename__ = 'planetas'
     id = Column(Integer, primary_key=True)
-    street_name = Column(String(250))
-    street_number = Column(String(250))
-    post_code = Column(String(250), nullable=False)
-    person_id = Column(Integer, ForeignKey('person.id'))
-    person = relationship(Person)
+    name = Column(String(250), nullable=False)
+    population = Column(Integer)
+    terrain = Column(String(250))
+    climate = Column(String(250))
+  
+
+
+
 
     def to_dict(self):
         return {}
